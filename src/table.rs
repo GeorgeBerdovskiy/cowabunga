0a7558d0a61eb27050cd3876ceee77a613a52a0e
GeorgeBerdovskiy <gaberdovskiy@gmail.com>
2024-01-29 12:23:55 -0800

    Restructure Cells, Columns, Table

--- src/table.rs
+++ src/table.rs
@@ -1,130 +1,159 @@
 use std::collections::HashMap;
-use std::marker::PhantomData;
-
 use pyo3::prelude::*;
+use pyo3::pyclass;
 
-/// Represents record IDs (RIDs), which are _physical_.
+/// Number of cells that can be stored in a page.
+const CELLS_PER_PAGE: usize = 512;
+
+/// Contains one record field. Because all fields are 64 bit integers, we use `i64`.
+/// If a field has been written, it contains `Some(i64)`. Otherwise, it holds `None`.
+#[derive(Copy, Clone)]
+struct Cell(Option<i64>);
+
+impl Cell {
+    /// Create a new cell.
+    pub fn new(value: Option<i64>) -> Self {
+        Cell(value)
+    }
+
+    /// Create a new **empty** cell.
+    pub fn empty() -> Self {
+        Cell(None)
+    }
+}
+
+/// Represents a physical page. In our design, every physical page has 512 cells. Therefore,
+/// each has a size of **4096 bytes.** In order to calculate the physical location of a record,
+/// we divide the RID by 512 for the page index and calculate the remainder for the offset (or
+/// cell index).
+struct Page {
+    /// Fixed size array of cells.
+    cells: [Cell; CELLS_PER_PAGE],
+
+    /// The number of cells currently written. Also represents the next available index.
+    cell_count: usize
+}
+
+impl Page {
+    /// Create a new empty physical page.
+    pub fn new() -> Self {
+        Page {
+            cells: [Cell::empty(); CELLS_PER_PAGE],
+            cell_count: 0
+        }
+    }
+
+    /// Write a new record to a physical page. Guaranteed to succeed.
+    pub fn write(&mut self, offset: usize, value: Option<i64>) {
+        self.cells[offset] = Cell::new(value);
+        self.cell_count += 1;
+    }
+}
+
+/// Represents the RID (record identifier) of a record. The struct isn't required, but benefits
+/// code readability and helps us catch errors using the type checker.
+/// 
+/// The RID is used to calculate the physical location of a page by dividing it by 512 for the
+/// **page index** and calculating `<RID> % 512` for the offset. We increment the RID by one for
+/// every new record so it's impossible to accidentally overwrite previous records.
 #[derive(Debug)]
 struct RID {
     /// Index of the page that contains this record
-    page: usize,
-
-    /// Index of the slot that contains this record
-    slot: usize
+    _inner: usize
 }
 
-/// Represents a table. According to the L-Store architecture, a table is really just an index,
-/// a collection of base pages, and a collection of tail pages.
+/// Represents a table, including all of its indexes, pages, and RID mappings.
 #[pyclass]
 pub struct Table {
-    /// Name of the table
+    /// The name of the table.
     name: String,
 
-    /// Map from logical IDs to record IDs
+    /// The number of columns.
+    num_columns: usize,
+
+    /// The LID (or _logical_ identifier) is a unique identifier that doesn't change as the table
+    /// is updated, while the RID (or _record_ identifier) represents the physical location of a record.
+    /// 
+    /// Every base and tail record is associated with an RID, and they share the same RID space.
+    /// 
+    /// When working with a logical record, our first step is to obtain the RID of its most recent update
+    /// using its logical identifier.
     lid_to_rid: HashMap<i64, RID>,
 
-    /// Number of columns in the table
-    number_of_columns: usize,
+    /// The next available **base** RID. We increment this field by one for every new base record.
+    next_base_rid: usize,
 
-    /// Set of table's base pages
-    base_pages: Vec<Page<Base>>,
+    /// The next available **tail** RID. We increment this field by one for every new base record.
+    next_tail_rid: usize,
 
-    /// Set of table's tail pages
-    tail_pages: Vec<Page<Tail>>
+    /// List of logical columns. See the definition of `Column` for more information
+    columns: Vec<Column>
+}
+
+struct Column {
+    base_pages: Vec<Page>,
+    tail_pages: Vec<Page>
+}
+
+impl Column {
+    pub fn new() -> Self {
+        Column {
+            base_pages: Vec::new(),
+            tail_pages: Vec::new()
+        }
+    }
+
+    pub fn insert_base(&mut self, page_index: usize, cell_index: usize, value: Option<i64>) {
+        self.base_pages[page_index].write(page_index, value);
+    }
 }
 
 #[pymethods]
 impl Table {
-    /// Create a new empty table.
     #[new]
     pub fn new(name: String, number_of_columns: usize) -> Self {
         Table {
-            name, number_of_columns,
+            name: name,
+            num_columns: number_of_columns,
             lid_to_rid: HashMap::new(),
-            base_pages: vec![Page {
-                columns: vec![ColumnPage::new(); number_of_columns],
-                phantom: PhantomData::<Base>
-            }],
-            tail_pages: vec![Page {
-                columns: vec![ColumnPage::new(); number_of_columns],
-                phantom: PhantomData::<Tail>
-            }]
+            next_base_rid: 0,
+            next_tail_rid: 0,
+            columns: Vec::new()
         }
     }
 
-    /// Format as a string for printing in Python.
-    pub fn __str__(&self) -> PyResult<String> {
-        let string = format!("{}\n---------\n", self.name);
-        let string = string + &format!("{:?}", self.lid_to_rid) + "\n------\n";
-        let string = string + &format!("{:?}", self.base_pages);
+    /// Insert a new record with the columns in `columns`. Note that values may be `None`.
+    // TODO - Move to `Query`
+    pub fn insert(&mut self, columns: Vec<Option<i64>>) {
+        let page_index = self.next_base_rid / CELLS_PER_PAGE;
+        let cell_index = self.next_base_rid % CELLS_PER_PAGE;
 
-        Ok(string)
+        for column in self.columns.iter_mut().zip(columns.iter()) {
+            column.0.insert_base(page_index, cell_index, *column.1);
+        }
+
+        self.next_base_rid += 1;
+        println!("[INFO] Inserted base record {:?} with RID {}.", columns, self.next_base_rid);
     }
 
-    /// Insert a new record. If the record is successfully inserted, return `Ok()`. Otherwise,
-    /// return `Err(String)`.
-    pub fn insert(&mut self, columns: Vec<i64>) -> PyResult<()> {
-        if columns.len() != self.number_of_columns {
-            return Err(PyErr::new::<pyo3::exceptions::PyValueError, _>(
-                "Wrong number of columns",
-            ));
-        }
-
-        // The first key is our LID - make sure it doesn't exist yet
-        if self.lid_to_rid.contains_key(&columns[0]) {
-            return Err(PyErr::new::<pyo3::exceptions::PyValueError, _>(
-                "Record with this ID already exists",
-            ));
-        }
-
-        let base_page_length = self.base_pages.len() - 1;
-        let page = &mut self.base_pages[base_page_length];
-
-        let next_col = columns.iter();
-        let mut next_col_index: usize = 0;
-
-        for col in next_col {
-            page.columns[next_col_index].insert_record(*col);
-            next_col_index += 1;
-        }
-
-        let page_index = base_page_length;
-        let slot_index = page.columns[0].records.len() - 1;
-        self.lid_to_rid.insert(columns[0], RID { page: page_index, slot: slot_index });
-
-        Ok(())
+    /// Select a logical record given its LID. Assume for now that `key` is the LID and is
+    /// always the first column.
+    // TODO - Move to `Query`
+    pub fn select(&mut self, key: i64) -> Record {
+        unimplemented!()
     }
 }
 
-/// Represents either a base or tail page. Note that the `Base` and `Tail` variants both contain
-/// `Vec<Column>` because they are _logical_ constructs. Physically, there is no difference between them.
-#[derive(Debug)]
-pub struct Base();
-
-#[derive(Debug)]
-pub struct Tail();
-
-#[derive(Debug)]
-pub struct Page<T> {
-    columns: Vec<ColumnPage>,
-    phantom: PhantomData<T>
+/// Represents the query interface.
+#[pyclass]
+struct Query {
+    /// The table we will interact with.
+    table: Table
 }
 
-/// Represents a column. You can think of a column as a "column page"
-#[derive(Debug)]
-#[derive(Clone)]
-pub struct ColumnPage {
-    records: Vec<i64>
-}
-
-impl ColumnPage {
-    pub fn new() -> Self {
-        ColumnPage {
-            records: Vec::new()
-        }
-    }
-
-    pub fn insert_record(&mut self, value: i64) {
-        self.records.push(value)
-    }
+/// Represents a **logical** record returned to the user in response to selection and
+/// range queries.
+#[pyclass]
+pub struct Record {
+    columns: Vec<Option<i64>>
 }
--- working_tester.py
+++ working_tester.py
@@ -5,9 +5,9 @@ from lstore.wrappers.table_wrapper import WrappedTable
 
 from ecs_165_database import table_module
 
-db = Database()
+'''db = Database()
 grades_table = db.create_table('Grades', 5, 0)
-query = Query(grades_table)
+query = Query(grades_table)'''
 
 rust_table = table_module.Table("Grades", 5)
 rust_table.insert([10110, 95, 96, 97, 98])
