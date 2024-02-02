1122550f5faf0bfe568598f43f28037dd6650135
Nate Buttke <nate-web@riseup.net>
2024-02-01 17:46:08 -0800

    move `Page` to bufferpool file. Lots of table changes yet to come.

--- src/bufferpool.rs
+++ src/bufferpool.rs
@@ -1,7 +1,69 @@
 // (Milestone One) Handle reads, writes, and creation of physical pages _in memory_ only.
-use crate::table::Page;
 
-struct BufferPool {
+/// Number of cells that can be stored in a page.
+const CELLS_PER_PAGE: usize = 512;
+
+/// Contains one record field. Because all fields are 64 bit integers, we use `i64`.
+/// If a field has been written, it contains `Some(i64)`. Otherwise, it holds `None`.
+#[derive(Copy, Clone, Debug)]
+struct Cell(Option<i64>);
+
+impl Cell {
+    /// Create a new cell.
+    pub fn new(value: Option<i64>) -> Self {
+        Cell(value)
+    }
+
+    /// Create a new **empty** cell.
+    pub fn empty() -> Self {
+        Cell(None)
+    }
+}
+
+
+/// Represents a physical page. In our design, every physical page has 512 cells. Therefore,
+/// each has a size of **4096 bytes**. In order to calculate the physical location of a record,
+/// we divide the RID by 512 for the page index and calculate the remainder for the offset (the
+/// cell index).
+#[derive(Clone, Copy, Debug)]
+pub struct Page {
+    /// Fixed size array of cells.
+    cells: [Cell; CELLS_PER_PAGE],
+
+    /// The number of cells currently written. Also represents the next available index.
+    cell_count: usize,
+}
+
+impl Page {
+    /// Create a new empty physical page.
+    pub fn new() -> Self {
+        Page {
+            cells: [Cell::empty(); CELLS_PER_PAGE],
+            cell_count: 0,
+        }
+    }
+
+    /// Write a new record to a physical page. For now, assume the offset is always valid.
+    pub fn write(&mut self, offset: usize, value: Option<i64>) -> Result<(), ()> {
+        if offset >= self.cells.len() {
+            return Err(());
+        }
+
+        self.cells[offset] = Cell::new(value);
+        self.cell_count += 1;
+
+        Ok(())
+    }
+
+    /// Read a single cell from a physical page. For now, assume the offset is always valid.
+    pub fn read(&self, offset: usize) -> Option<i64> {
+        // TODO: Ensure that `offset` is within the acceptable range and return an error if it isn't.
+        self.cells[offset].0
+    }
+}
+
+
+pub struct BufferPool {
     pages: Vec<Page>
 }
 
