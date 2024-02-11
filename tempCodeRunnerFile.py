2251a40683ae8c42671e4c876406ad4b75973b59
Kevin Bao <kevinbao03@gmail.com>
2024-02-10 16:29:11 -0800

    added select_version

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,39 +1,3 @@
-from lstore.db import Database
-from lstore.query import Query
-
-from random import choice, randint, sample, seed
-
-db = Database()
-# Create a table  with 5 columns
-#   Student Id and 4 grades
-#   The first argument is name of the table
-#   The second argument is the number of columns
-#   The third argument is determining the which columns will be primay key
-#       Here the first column would be student id and primary key
-grades_table = db.create_table('Grades', 5, 0)
-
-# create a query class for the grades table
-query = Query(grades_table) 
-
-# dictionary for records to test the database: test directory
-records = {}
-
-number_of_records = 1000
-number_of_aggregates = 100
-seed(3562901)
-
-for i in range(0, number_of_records):
-    key = 92106429 + randint(0, number_of_records)
-
-    #skip duplicate keys
-    while key in records:
-        key = 92106429 + randint(0, number_of_records)
-
-    records[key] = [key, randint(0, 20), randint(0, 20), randint(0, 20), randint(0, 20)]
-    query.insert(*records[key])
-    # print('inserted', records[key])
-print("Insert finished")
-
 # Check inserted records using select query
 for key in records:
     # select function will return array of records 
