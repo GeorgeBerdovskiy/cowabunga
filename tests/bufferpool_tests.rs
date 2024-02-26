0f3a79de49eec5611a97750c13315a8ee2cd06d7
Nate Buttke <nate-web@riseup.net>
2024-02-25 17:03:28 -0800

    test to fill all frames

--- tests/bufferpool_tests.rs
+++ tests/bufferpool_tests.rs
@@ -1,10 +1,11 @@
 extern crate cowabunga_rs;
 use cowabunga_rs::bufferpool::*;
+use cowabunga_rs::constants::*;
 
+/// Simplest conceivable write_next_value() use
 #[test]
-fn test_simple_write() {
-    // Setup test, e.g., create a new instance of the part of your library you're testing.
-    let mut bp = BufferPool::new(); // Adjust according to your actual API
+fn test_simple_write_next_value()  -> Result<(), String> {
+    let mut bp = BufferPool::new();
 
     let ppid = bp.allocate_page("test_tbl".to_string(), 0);
 
@@ -12,4 +13,33 @@ fn test_simple_write() {
     let val_read = bp.read(ppid, offset);
 
     assert_eq!(val_read, 42);
+    Ok(())
+}
+
+
+/// Force an eviction to occur using allocate, reads and writes
+#[test]
+fn test_full_bufferpool()  -> Result<(), String> {
+    let mut bp = BufferPool::new();
+
+    let mut ppids: Vec<PhysicalPageID> = Vec::new();
+
+    // create more pages than frames (all in one column)
+    for _ in (1..=BP_NUM_FRAMES+1) {
+        ppids.push(bp.allocate_page("test_tbl".to_string(), 0));
+    }
+
+    for (i, ppid) in ppids.iter().enumerate() {
+        bp.write_next_value(*ppid, Some(i as i64 + 1));
+    }
+
+    let expected_values: Vec<i64> = (1..=BP_NUM_FRAMES+1).collect();
+    let mut observed_values: Vec<i64> = Vec::new();
+
+    for ppid in &ppids {
+        observed_values.push(bp.read(*ppid, 0)?);
+    }
+
+    assert_eq!(expected_values, observed_values);
+    Ok(())
 }
